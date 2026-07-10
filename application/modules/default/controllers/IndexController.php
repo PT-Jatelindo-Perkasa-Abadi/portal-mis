806f7b18c391060e7f9d9e9f5e9fbac4da7bf4e9
adetrihadi <adetrihadi@jatelindo.co.id>
2026-07-10 09:03:56 +0700

    update: add card & chart summary transaction

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -1,5 +1,5 @@
 <?php
-class Default_IndexController extends Zend_Controller_Action
+class Default_IndexController extends App_Controller_Base
 {
     public function init()
     {
@@ -8,5 +8,58 @@ class Default_IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         $this->view->headTitle('Dashboard');
+        $this->view->headScript()->appendFile($this->view->baseUrl('/assets/js/chart.umd.min.js'));
+        $this->view->headScript()->appendFile($this->view->baseUrl('/assets/js/dashboard-chart.js'));
+        $this->view->headScript()->appendFile($this->view->baseUrl('/assets/js/format-currency-compact.js'));
+        $this->view->headScript()->appendFile($this->view->baseUrl('/assets/js/format-number.js'));
+    }
+
+    public function totalSummaryAction() {
+        $service = $this->api();
+        $response = $service->request(
+            'POST',
+            '/service/proxy/service/alias/row1-all-tp-now',
+            ["conf" => "ch_12_dev"]
+        );
+
+        return $this->jsonSuccess($response['msg'][0]);
+    }
+
+    public function summaryTransactionAverageAction() {
+        $service = $this->api();
+        $response = $service->request(
+            'POST',
+            '/service/proxy/service/alias/row2-all-tp-yesterday',
+            ["conf" => "mis_ch_rekon"]
+        );
+
+        return $this->jsonSuccess($response['msg'][0]);
+    }
+
+    public function transactionChartAction() {
+        $isDay = $this->_getParam('currentDate');
+        $service = $this->api();
+        $response = [];
+
+        if ($isDay == '1') {
+            $response = $service->request(
+                'POST',
+                '/service/proxy/service/alias/row3-all-tp-now',
+                ["conf" => "ch_12_dev"]
+            );
+        }
+
+        if ($isDay == '0') {
+            $response = $service->request(
+                'POST',
+                '/service/proxy/service/alias/row3-all-tp-yesterday',
+                ["conf" => "ch_12_dev"]
+            );
+        }
+
+        Zend_Debug::dump($response);
+        exit;
+
+        return $this->jsonSuccess($response['msg'][0]);
     }
 }
